0b5d957787e05fac14b8b224995b642198940541
Tina Ellis <tina.ellis@getg5.com>
2015-03-16 08:08:55 -0700

    Add calculator file

---
+++ calculator.rb
@@ -0,0 +1,26 @@
+def say(msg)
+  puts "=> #{msg}"
+end
+
+say "What's the first number?"
+num1 = gets.chomp
+
+say "What's the second number?"
+num2 = gets.chomp
+
+say "1) add 2) subtract 3) multiply 4) divide"
+operator = gets.chomp
+
+if operator == '1'
+  result = num1.to_i + num2.to_i
+elsif operator == '2'
+  result = num1.to_i - num2.to_i
+elsif operator == '3'
+  result = num1.to_i * num2.to_i
+elsif operator == '4'
+  result = num1.to_f / num2.to_f
+else
+  puts "Did not recognize a number between 1-4. Sorry."
+end
+
+puts "Result is #{result}"
