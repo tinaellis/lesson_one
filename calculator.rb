d05c3c990880e2a6dce3c6d353e4637b03564c15
Tina Ellis <tina.ellis@getg5.com>
2015-03-17 06:17:12 -0700

    Refactor method and while loop

--- calculator.rb
+++ calculator.rb
@@ -1,42 +1,39 @@
-def say(msg)
-  puts "=> #{msg}"
+def user_input(record)
+  puts "=> What's the #{record} number?"
+  gets.chomp
 end
 
 while true do
-  say "What's the first number?"
-  num1 = gets.chomp
+  num1 = user_input("first")
+  num2 = user_input("second")
 
-  say "What's the second number?"
-  num2 = gets.chomp
+  begin
+    puts "=> What would you like to do? 1) add 2) subtract 3) multiply 4) divide"
+    operator = gets.chomp
+  end until %w(1 2 3 4).include?(operator)
 
-  say "1) add 2) subtract 3) multiply 4) divide"
-  operator = gets.chomp
-
-  if operator == '1'
-    result = num1.to_i + num2.to_i
-  elsif operator == '2'
-    result = num1.to_i - num2.to_i
-  elsif operator == '3'
-    result = num1.to_i * num2.to_i
-  elsif operator == '4'
-    result = num1.to_f / num2.to_f
-  else
-    puts "Did not recognize a number between 1-4. Sorry."
+  result = case operator
+    when '1' then num1.to_i + num2.to_i
+    when '2' then num1.to_i - num2.to_i
+    when '3' then num1.to_i * num2.to_i
+    when '4' then num1.to_f / num2.to_f
   end
 
   puts "Result is #{result}"
   puts "Would you like to perform another calculation? (Y/N)"
-  rep_calc = gets.chomp.upcase
+  calculate_again = gets.chomp.upcase
 
-  until rep_calc == 'Y' || rep_calc == 'N'
+  until calculate_again == 'Y' || calculate_again == 'N'
     puts "Invalid input. Try again."
     puts "Would you like to perform another calculation? (Y/N)"
-    rep_calc = gets.chomp.upcase
+    calculate_again = gets.chomp.upcase
   end
 
-  if rep_calc == 'N'
-    break
-  elsif rep_calc == 'Y'
-    true
+  if calculate_again == 'N'
+      break
+  elsif calculate_again == 'Y'
+      true
   end
 end
+
+puts "Good bye!"
