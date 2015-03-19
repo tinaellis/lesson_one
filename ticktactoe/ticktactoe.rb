e74d89540b06c94522b92337ab89d0a63a0522ee
Tina Ellis <tina.ellis@getg5.com>
2015-03-19 05:39:51 -0700

    Resolve hash - array issue

--- ticktactoe/ticktactoe.rb
+++ ticktactoe/ticktactoe.rb
@@ -1,35 +1,49 @@
-#1. come up with the requirements/specifications, which will determine the scope
-#2. application logic: sequence of steps
-#3. translation of those steps into code
-#4. run tests to verify logic
-
-# welcome message / computer is o, player is x
-# Draw a board
-# X’s or O’s
-
-# loop until a winner or all squares are taken
-# player 1 pics empty square
-# check for winner
-# player 2 pics empty square
-# check for winner
-
-# It’s a tie
-# Player 1 wins
-# Player 2 wins
-# Play again?
-
-"Welcome to Tick Tack Toe! You are automatically assigned to 'x', the computer will be playing
-as 'o'. On your mark, get set, go!"
-
 def initialize_board
-  b = {}
-  (1..9).each {|position| b[position] = ' '}
-  b
+  board = {}
+  (1..9).each {|position| board[position] = ' '}
+  board
+end
+
+def empty_positions(board)
+  board.keys.select {|position| board[position] == ' ' }
+end
+
+def player_picks_square(board)
+  begin
+    puts "Choose a position (from 1 to 9) to place a piece:"
+    position = gets.chomp.to_i
+  end until empty_positions(board).include?(position)
+  board[position] = 'X'
+end
+
+def computer_picks_square(board)
+  position = empty_positions(board).sample
+  board[position] = 'o'
+end
+
+def check_winner(board)
+  winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+  winning_lines.each do |line|
+    return "Player" if board.values_at(*line).count('X') == 3
+    return "Computer" if board.values_at(*line).count('O') == 3
+  end
+  nil
+end
+
+def nine_positions_are_filled?(board)
+  empty_positions(board) == []
+end
+
+def announce_winner(winner)
+  puts "#{winner} won!"
 end
 
 def draw_board(b)
   system "clear"
   #{b[1]}
+  puts " ___ _ ____ _  _   ___ ____ ____   ___ ____ ____"
+  puts "  |  | |___ |-:_    |  |--| |___    |  [__] |==="
+  puts " "
   puts "      |     |     "
   puts "   #{b[1]}  |  #{b[2]}  |  #{b[3]}  "
   puts "      |     |     "
@@ -43,48 +57,17 @@ def draw_board(b)
   puts "      |     |     "
 end
 
-def empty_positions(b)
-  b.select {|k, v| v == ' ' }.keys
-end
-
-def player_picks_square(b)
-  #add verification that player picks a valid square
-  puts "pick a square (1 - 9)"
-  position = gets.chomp.to_i
-  b[position] = 'x'
-end
-
-def computer_picks_square(b)
-  position = empty_positions(b).sample
-  b[position] = 'o'
-end
-
-def check_winner(b)
-  winning_lines = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
-  winning_lines.each do |line|
-    if b[line[0]] == 'x' and b[line[1]] == 'x' and b[line[2]] == 'x'
-      return "Player"
-    elsif b[line[0]] == 'o' and b[line[1]] == 'o' and b[line[2]] == 'o'
-      return "Computer"
-    else
-      return nil
-    end
-  end
-end
-
 board = initialize_board
 draw_board(board)
-
 begin
   player_picks_square(board)
+  draw_board(board)
   computer_picks_square(board)
   draw_board(board)
   winner = check_winner(board)
-end until winner || empty_positions(board).empty?
-
+end until winner || nine_positions_are_filled?(board)
 if winner
-  puts "#{winner} won!"
+  announce_winner(winner)
 else
-  puts "It's a tie!"
+  puts "It's a tie."
 end
-
