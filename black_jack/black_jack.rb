44325c662da19c40d2324f9335823ceb5bb224bb
Tina Ellis <tina.ellis@getg5.com>
2015-03-24 08:38:14 -0700

    Add conditional for blackjack option

--- black_jack/black_jack.rb
+++ black_jack/black_jack.rb
@@ -56,40 +56,49 @@ begin
   puts "The dealer's cards are #{dealer_cards}, for a total of #{calculate_total(dealer_cards)}"
   puts ""
 
-  say "#{player_name} would you like to 1) hit or 2) stay"
-  hit_or_stay = gets.chomp.to_i
-
-  if hit_or_stay == 1 #hit
-    begin
-      player_cards << deck.pop
-      puts "#{player_name}'s cards are #{player_cards}, for a total of #{calculate_total(player_cards)}"
-      puts "#{player_name} Would you like to 1) hit or 2) stay"
-      hit_or_stay = gets.chomp.to_i
-    end until hit_or_stay == 2 || calculate_total(player_cards) > 21
-  end
-
-  if calculate_total(dealer_cards) < 17
-    begin
-      dealer_cards << deck.pop
-      puts "The dealer takes a card."
-    end until calculate_total(dealer_cards) >= 17
-  end
-
-  puts "Comparing hands..."
-  puts "#{player_name}'s cards are #{player_cards}, for a total of #{calculate_total(player_cards)}"
-  puts "The dealer's cards are #{dealer_cards}, for a total of #{calculate_total(dealer_cards)}"
-  puts ""
-
-  if calculate_total(dealer_cards) > 21
-    puts "#{player_name} won!"
-  elsif calculate_total(player_cards) > 21 || calculate_total(player_cards) < calculate_total(dealer_cards)
-    puts "House won."
-  elsif calculate_total(player_cards) > calculate_total(dealer_cards) || calculate_total(dealer_cards) < 21
-    puts "#{player_name} won!"
+  # Player Blackjack
+  if calculate_total(player_cards) == 21
+    puts "Congratulations, you hit blackjack! You win!"
   else
-    puts "It's a tie!"
-  end
+    say "#{player_name} would you like to 1) hit or 2) stay"
+    hit_or_stay = gets.chomp.to_i
 
-  say "#{player_name}, would you like to play again? Y/N"
-  play_again = gets.chomp.downcase
+    if hit_or_stay == 1 #hit
+      begin
+        player_cards << deck.pop
+        puts "#{player_name}'s cards are #{player_cards}, for a total of #{calculate_total(player_cards)}"
+        puts "#{player_name} Would you like to 1) hit or 2) stay"
+        hit_or_stay = gets.chomp.to_i
+      end until hit_or_stay == 2 || calculate_total(player_cards) > 21
+    end
+
+    if calculate_total(dealer_cards) < 17
+      begin
+        dealer_cards << deck.pop
+        puts "The dealer takes a card."
+      end until calculate_total(dealer_cards) >= 17
+    end
+
+    puts "Comparing hands..."
+    puts "#{player_name}'s cards are #{player_cards}, for a total of #{calculate_total(player_cards)}"
+    puts "The dealer's cards are #{dealer_cards}, for a total of #{calculate_total(dealer_cards)}"
+    puts ""
+
+    if calculate_total(dealer_cards) > 21
+      puts "#{player_name} won!"
+    elsif calculate_total(player_cards) > 21 || calculate_total(player_cards) < calculate_total(dealer_cards)
+      puts "House won."
+    elsif calculate_total(player_cards) > calculate_total(dealer_cards) || calculate_total(dealer_cards) < 21
+      puts "#{player_name} won!"
+    else
+      puts "It's a tie!"
+    end
+
+  end # End Player blackjack
+
+    say "#{player_name}, would you like to play again? Y/N"
+    play_again = gets.chomp.downcase
+
 end until play_again == "n"
+
+puts "Good bye."
