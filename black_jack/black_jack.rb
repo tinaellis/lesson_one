735a66e7124c19a1162648e4c06fef4ed9575950
Tina Ellis <tina.ellis@getg5.com>
2015-03-25 05:58:44 -0700

    Clean up method and constants

--- black_jack/black_jack.rb
+++ black_jack/black_jack.rb
@@ -34,30 +34,47 @@ def calculate_total(cards) #[['heart','2'],['diamond','3']]
   total
 end
 
-suits = ['Heart', 'Diamond', 'Spade', 'Clubs']
-cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-deck = suits.product(cards)
-deck.shuffle!
+def shuffle_cards
+  puts("|S|h|u|f|f|l|i|n|g| |C|a|r|d|s|")
+  3.times do
+    print ". . . . . . . . . . . . "
+    sleep 0.5
+  end
+end
 
+def delay
+  sleep 1
+end
+
+BLACKJACK = 21
+SUITS = ['Heart', 'Diamond', 'Spade', 'Clubs']
+CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+deck = SUITS.product(CARDS)
+deck.shuffle!
 player_name = player_one
 
 begin
-  player_cards = []
-  dealer_cards = []
-  puts "-- START --"
+  shuffle_cards
+  puts ""
   puts "Welcome to Blackjack #{player_name}. You and the dealer are both dealt two cards to start the game."
 
-  player_cards << deck.pop
-  dealer_cards << deck.pop
-  player_cards << deck.pop
-  dealer_cards << deck.pop
+  player_cards = []
+  dealer_cards = []
 
-  puts "#{player_name}'s cards are #{player_cards}, for a total of #{calculate_total(player_cards)}"
-  puts "The dealer's cards are #{dealer_cards}, for a total of #{calculate_total(dealer_cards)}"
+  2.times do
+    player_cards << deck.pop
+    dealer_cards << deck.pop
+  end
+
+  player_total = calculate_total(player_cards)
+  dealer_total = calculate_total(dealer_cards)
+
+  puts "#{player_name}'s cards are #{player_cards}, for a total of #{player_total}"
+  puts "The dealer's card is #{dealer_cards[0]}"
   puts ""
 
   # Player Blackjack
-  if calculate_total(player_cards) == 21
+  if calculate_total(player_cards) == BLACKJACK
     puts "Congratulations, you hit blackjack! You win!"
   else
     say "#{player_name} would you like to 1) hit or 2) stay"
@@ -66,38 +83,41 @@ begin
     if hit_or_stay == 1 #hit
       begin
         player_cards << deck.pop
-        puts "#{player_name}'s cards are #{player_cards}, for a total of #{calculate_total(player_cards)}"
+        player_total = calculate_total(player_cards)
+        puts "#{player_name}'s cards are #{player_cards}, for a total of #{player_total}"
         puts "#{player_name} Would you like to 1) hit or 2) stay"
         hit_or_stay = gets.chomp.to_i
-      end until hit_or_stay == 2 || calculate_total(player_cards) > 21
+      end until hit_or_stay == 2 || player_total > BLACKJACK
     end
 
-    if calculate_total(dealer_cards) < 17
+    if dealer_total < 17
       begin
         dealer_cards << deck.pop
+        dealer_total = calculate_total(dealer_cards)
         puts "The dealer takes a card."
-      end until calculate_total(dealer_cards) >= 17
+        delay
+      end until dealer_total >= 17
     end
 
     puts "Comparing hands..."
-    puts "#{player_name}'s cards are #{player_cards}, for a total of #{calculate_total(player_cards)}"
-    puts "The dealer's cards are #{dealer_cards}, for a total of #{calculate_total(dealer_cards)}"
+    delay
+    puts "#{player_name}'s cards are #{player_cards}, for a total of #{player_total}"
+    puts "The dealer's cards are #{dealer_cards}, for a total of #{dealer_total}"
     puts ""
 
-    if calculate_total(dealer_cards) > 21
+    if dealer_total > BLACKJACK
       puts "#{player_name} won!"
-    elsif calculate_total(player_cards) > 21 || calculate_total(player_cards) < calculate_total(dealer_cards)
+    elsif player_total > BLACKJACK || player_total < dealer_total
       puts "House won."
-    elsif calculate_total(player_cards) > calculate_total(dealer_cards) || calculate_total(dealer_cards) < 21
+    elsif player_total > dealer_total || dealer_total < BLACKJACK
       puts "#{player_name} won!"
     else
       puts "It's a tie!"
     end
-
   end # End Player blackjack
 
-    say "#{player_name}, would you like to play again? Y/N"
-    play_again = gets.chomp.downcase
+  say "#{player_name}, would you like to play again? Y/N"
+  play_again = gets.chomp.downcase
 
 end until play_again == "n"
 
